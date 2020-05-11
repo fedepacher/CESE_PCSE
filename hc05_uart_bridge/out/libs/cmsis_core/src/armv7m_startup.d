deaa07836ae6f52e5fef8e3d7a57db347cf926dc
Fede Pacher <fede_pacher@hotmail.com>
2020-05-11 18:26:35 -0300

    modificacione x

--- hc05_uart_bridge/out/libs/cmsis_core/src/armv7m_startup.d
+++ hc05_uart_bridge/out/libs/cmsis_core/src/armv7m_startup.d
@@ -1,2 +1,2 @@
-Fede//hc05_uart_bridge/out/libs/cmsis_core//src/armv7m_startup.o: \
+Fede/Protocolos/CESE_PCSE/hc05_uart_bridge/out/libs/cmsis_core//src/armv7m_startup.o: \
  libs/cmsis_core//src/armv7m_startup.c
